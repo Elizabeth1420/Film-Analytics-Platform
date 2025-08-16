cb50a102f5a9f58c2a5181c52bc7f85dcad67398
fjc-22233580 <22233580@bucks.ac.uk>
2025-08-16 21:04:51 +0100

    Added year to search

--- backend/src/controllers/search.controllers.js
+++ backend/src/controllers/search.controllers.js
@@ -6,12 +6,13 @@ const { handleApiError } = require("../utils/apiUtils");
 async function getMovieByName(req, res, next) {
   try {
     const movieName = req.query.name;
+    const year = req.query.year;
     const page = Math.min(
       Math.max(parseIntWithFallback(req.query.page, 1), 1),
       500
     );
 
-    const data = await searchService.fetchSearch(movieName, page);
+    const data = await searchService.fetchSearch(movieName, year ,page);
     res.status(200).json(data);
   } catch (err) {
     handleApiError(err, res, next);
--- backend/src/service/search.service.js
+++ backend/src/service/search.service.js
@@ -1,10 +1,11 @@
 const { tmdbFetch } = require("../utils/apiUtils");
 
-async function fetchSearch(query, page = 1) {
+async function fetchSearch(query, year, page = 1) {
   
   const params = new URLSearchParams({    
     include_adult: "false",
     language: "en-US",
+    primary_release_year: year,
     query: query,
     page: String(page),
   });
