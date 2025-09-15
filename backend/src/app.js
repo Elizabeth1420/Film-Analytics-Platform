3de809105e3dbe65548da3bceba75c2b8c00ce37
fjc-22233580 <22233580@bucks.ac.uk>
2025-09-15 21:27:40 +0100

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,10 @@ EnvHelper.validate();
 const app = express();
 app.use(express.json());
 
+// Use CORS to allow cross-origin requests
+const cors = require('cors');
+app.use(cors());
+
 // Expose the movies routes
 app.use('/api/movies', require('./routes/movies.routes'));
 
